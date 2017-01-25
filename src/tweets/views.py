4b759f33ebb040f805b9aa2c2f3a6cd14c885a3a
Abhishek Ghosh <abhighosh18@gmail.com>
2017-01-24 17:35:18 -0800

    Feat: Implement Class based generic views

--- src/tweets/views.py
+++ src/tweets/views.py
@@ -2,27 +2,43 @@
 Views for Tweets app
 '''
 from django.shortcuts import render
+from django.views.generic import DetailView, ListView
 from .models import Tweet
 # Create your views here.
 
-def tweet_detail_view(request, id=1):
-    '''
-    Detail View for tweet
-    '''
-    obj = Tweet.objects.get(id=id)
-    # print obj
-    context = {
-        "object": obj
-    }
-    return render(request, "tweets/detail_view.html", context)
-
-def tweet_list_view(request, id=1):
-    '''
-    Detail List for tweet
-    '''
+class TweetDetailView(DetailView):
     queryset = Tweet.objects.all()
-    # print queryset
-    context = {
-        "object_list": queryset
-    }
-    return render(request, "tweets/list_view.html", context)
+    def get_object(self):
+        return Tweet.objects.get(id=1)
+
+class TweetListView(ListView):
+    queryset = Tweet.objects.all()
+
+    # custom method to give a context
+    def get_context_data(self, *args, **kwargs):
+        context = super(TweetListView, self).get_context_data(*args, **kwargs)
+        # context["abhi"] = "Ghosh"
+        # print context
+        return context
+
+# def tweet_detail_view(request, id=1):
+#     '''
+#     Detail View for tweet
+#     '''
+#     obj = Tweet.objects.get(id=id)
+#     # print obj
+#     context = {
+#         "object": obj
+#     }
+#     return render(request, "tweets/detail_view.html", context)
+#
+# def tweet_list_view(request, id=1):
+#     '''
+#     Detail List for tweet
+#     '''
+#     queryset = Tweet.objects.all()
+#     # print queryset
+#     context = {
+#         "object_list": queryset
+#     }
+#     return render(request, "tweets/list_view.html", context)
