9d56f4cb43a5e0b33d7e5526004a7875ec605e8c
Abhishek Ghosh <abhighosh18@gmail.com>
2017-01-28 18:36:28 -0800

    Feat: Make the Create Tweet View

--- src/tweets/urls.py
+++ src/tweets/urls.py
@@ -2,13 +2,20 @@
 tweetAPP URL Configuration
 """
 from django.conf.urls import url
-from .views import TweetListView, TweetDetailView
-# tweet_list_view, tweet_detail_view,
+from .views import (
+    TweetListView,
+    TweetDetailView,
+    TweetCreateView,
+    )
+# tweet_list_view, tweet_detail_view, tweet_create_view
+
 
 urlpatterns = [
     #url(r'^admin/', admin.site.urls),
     # url(r'^$', tweet_list_view, name='list'),
     # url(r'^1/$', tweet_detail_view, name='detail'),
-    url(r'^$', TweetListView.as_view(), name='list'),    # /tweet/
+    # url(r'^create/$', tweet_create_view, name='create'),
+    url(r'^create/$', TweetCreateView.as_view(), name='create'),      # /tweet/create
+    url(r'^$', TweetListView.as_view(), name='list'),                 # /tweet/
     url(r'^(?P<id>\d+)/$', TweetDetailView.as_view(), name='detail'), # /tweet/1
 ]
